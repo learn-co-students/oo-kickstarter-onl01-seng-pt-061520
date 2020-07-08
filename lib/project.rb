b0077952f5306f2e2a0535fea44d3ff60ab24742
IgoVeyner <igorveyner95@gmail.com>
2020-07-08 11:20:24 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Project
+    attr_reader :title
+    attr_accessor :backers
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def add_backer(backer)
+        backers << backer
+        backer.back_project(self) unless backers.include?(self)
+    end
+end
